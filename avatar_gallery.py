93d93df512c4ac541bdd96a722e07172edf70b7e
Varuna108 <varuna108@yandex.ru>
2019-06-26 20:25:42 +0300

    new version

--- avatar_gallery.py
+++ avatar_gallery.py
@@ -1,49 +1,31 @@
 from PIL import Image
 
 img = Image.open("monro.jpeg")
-print(img.mode)
-
-# test convert cmyk to rgb
-
-img2 = Image.open("cmyk.jpg")
-print(img2.mode)
-img2rgb = img2.convert("RGB")
-print(img2rgb.mode)
 
 red, green, blue = img.split()
 
-# red channel
+first_crop = 100
+second_crop = 50
 
 new_red = red
-
-coordinates_red = (100, 0, new_red.width, new_red.height)
+coordinates_red = (first_crop, 0, new_red.width, new_red.height)
 cropped_red = new_red.crop(coordinates_red)
-
-coordinates2_red = (50, 0, red.width - 50, red.height)
+coordinates2_red = (second_crop, 0, red.width - second_crop, red.height)
 cropped2_red = red.crop(coordinates2_red)
-
-image_red = Image.blend(cropped_red, cropped2_red, 0.5)  # image_red - red channel
-
-# blue channel
+image_red = Image.blend(cropped_red, cropped2_red, 0.5)
 
 new_blue = blue
-
-coordinates_blue = (0, 0, new_blue.width - 100, new_blue.height)
+coordinates_blue = (0, 0, new_blue.width - first_crop, new_blue.height)
 cropped_blue = new_blue.crop(coordinates_blue)
-
-coordinates2_blue = (50, 0, blue.width - 50, blue.height)
+coordinates2_blue = (second_crop, 0, blue.width - second_crop, blue.height)
 cropped2_blue = blue.crop(coordinates2_blue)
+image_blue = Image.blend(cropped_blue, cropped2_blue, 0.5)
 
-image_blue = Image.blend(cropped_blue, cropped2_blue, 0.5)  # image_blue - blue channel
+coordinates_green = (second_crop, 0, green.width - second_crop, red.height)
+image_green = green.crop(coordinates_green)
 
-# green channel
-
-coordinates_green = (50, 0, green.width - 50, red.height)
-image_green = green.crop(coordinates_green)  # cropped_green - green channel
-
-final_effect = Image.merge("RGB", (image_red, image_green, image_blue))
-
-print(final_effect.width, final_effect.height)
-final_effect.thumbnail((80, 70))
-final_effect.show()
-final_effect.save("final.jpg", format="JPEG")
+size = (80, 80)
+your_image = Image.merge("RGB", (image_red, image_green, image_blue))
+your_image.thumbnail(size)
+your_image.show()
+your_image.save("your_image.jpg", format="JPEG")
